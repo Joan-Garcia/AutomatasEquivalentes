a3339ffc3da0e55bcacbd90c85fc0b3841035cbd
MiguelLPmez <mig.angel.1916@gmail.com>
2020-03-22 20:32:57 -0600

    Pruebas de la clase Automata - ML

---
+++ AutomatasEquivalentes/src/pruebas/PruebaAutómata.java
@@ -0,0 +1,19 @@
+package pruebas;
+
+import estructuras.Automata;
+
+public class PruebaAutómata {
+  
+  public static void main(String[] args) {
+    String[] alfabeto = {"a", "b"};
+    String[] estados = {"q0", "q1", "q2"};
+    String[] estadosFinales = {"q0", "q2"};
+    String estadoInicial = "q0";
+    
+    
+    Automata automata = new Automata(alfabeto, estados, estadosFinales, 
+                                     estadoInicial);
+    System.out.println(automata.getEstadoSiguiente("q2", "a"));
+    System.out.println(automata.getTablaTransiciones().toString());
+  }
+}
